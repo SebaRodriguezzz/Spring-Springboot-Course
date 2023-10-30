5d3a6078218cb08382886e60aa4e20c33cef212d
SebaRodriguezzz <sebas.r1070@gmail.com>
2023-10-30 01:42:23 -0300

    Change root directory of project

---
+++ movie-recommender-system/src/main/java/io/datajek/spring/basics/movierecommendersystem/lesson1/RecommenderImplementation.java
@@ -0,0 +1,10 @@
+package io.datajek.spring.basics.movierecommendersystem.lesson1;
+
+public class RecommenderImplementation {
+
+    public String[] recommendMovies(String movie){
+        ContentBasedFilter filter = new ContentBasedFilter();
+        String[] results = filter.getRecommendations(movie);
+        return results;
+    }
+}
---
+++ movie-recommender-system/src/main/java/io/datajek/spring/basics/movierecommendersystem/lesson15/ContentBasedFilter.java
@@ -0,0 +1,12 @@
+package io.datajek.spring.basics.movierecommendersystem.lesson15;
+
+import org.springframework.stereotype.Component;
+
+@Component("CBF")
+public class ContentBasedFilter implements Filter {
+
+    public String[] getRecommendations(String movie){
+        return new String[] {"Happy Feet", "Ice Age", "Shark Tale"};
+    }
+
+}
