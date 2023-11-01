212f82b85a9d30463f5100c644518de2503b5a8f
SebaRodriguezzz <sebas.r1070@gmail.com>
2023-11-01 01:04:35 -0300

    Use of JDBC and RowMapper w UPDATE  query

--- tennis-player/src/main/java/springdata/tennisplayer/TennisPlayerApplication.java
+++ tennis-player/src/main/java/springdata/tennisplayer/TennisPlayerApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.Date;
+import java.sql.Date;
 
 @SpringBootApplication
 public class TennisPlayerApplication implements CommandLineRunner {
@@ -21,12 +21,15 @@ public class TennisPlayerApplication implements CommandLineRunner {
 	}
 
 	@Override
-	public void run(String... args) throws Exception
-	{
+	public void run(String... args) throws Exception {
 		logger.info("Inserting Player 4: {}", dao.insertPlayer(
-				new Player (4, "Thiem", "Austria",
-						new Date(System.currentTimeMillis()),
-						17 )));
-		logger.info("All Players Data: {}", dao.getAllPlayers());
+				new Player(4, "Thiem", "Austria",
+						new Date(System.currentTimeMillis()), 17)));
+
+		logger.info("Updating Player with Id 4: {}",  dao.updatePlayer(
+				new Player(4, "Thiem", "Austria",
+						Date.valueOf("1993-09-03"), 17)));
+
+		logger.info("Players with Id 4: {}", dao.getPlayerById(4));
 	}
 }
