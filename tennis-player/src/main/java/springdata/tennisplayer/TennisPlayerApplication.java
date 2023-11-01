5bf26a9e4114938516bef982f122c4833b2bb513
SebaRodriguezzz <sebas.r1070@gmail.com>
2023-11-01 01:20:07 -0300

    Created custom RowMapper

--- tennis-player/src/main/java/springdata/tennisplayer/TennisPlayerApplication.java
+++ tennis-player/src/main/java/springdata/tennisplayer/TennisPlayerApplication.java
@@ -22,6 +22,6 @@ public class TennisPlayerApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		dao.createTournamentTable();
+		logger.info("French Players: {}", dao.getPlayerByNationality("France"));
 	}
 }
