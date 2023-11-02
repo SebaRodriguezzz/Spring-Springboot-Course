dc3c29ed7728cc573645f2bd6225ad8bea343429
SebaRodriguezzz <sebas.r1070@gmail.com>
2023-11-02 01:28:21 -0300

    Switch to JPA

---
+++ tennis-player/src/main/java/io/datajek/springdatajpa/TennisPlayerApplication.java
@@ -0,0 +1,18 @@
+package io.datajek.springdatajpa;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springdata.tennisplayer.PlayerDao;
+
+@SpringBootApplication
+public class TennisPlayerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TennisPlayerApplication.class, args);
+	}
+
+}
