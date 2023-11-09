159395e6a8f6c3896cae7a01cc8804286b28cc67
SebaRodriguezzz <sebas.r1070@gmail.com>
2023-11-09 01:22:53 -0300

    Add endpoint with Path Variable

--- tennis-player-rest/src/main/java/io/datajek/tennisplayerrest/PlayerController.java
+++ tennis-player-rest/src/main/java/io/datajek/tennisplayerrest/PlayerController.java
@@ -2,6 +2,7 @@ package io.datajek.tennisplayerrest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -15,6 +16,11 @@ public class PlayerController {
     @GetMapping("/welcome")
     public String welcome(){
         return "Tennis Player REST API";
+    }
+
+    @GetMapping("/players/{id}")
+    public Player getPlayer(@PathVariable int id){
+        return service.getPlayer(id);
     }
 
     @GetMapping("/players")
--- tennis-player-rest/src/main/java/io/datajek/tennisplayerrest/PlayerService.java
+++ tennis-player-rest/src/main/java/io/datajek/tennisplayerrest/PlayerService.java
@@ -4,11 +4,27 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PlayerService {
     @Autowired
     PlayerRepository repo;
+
+    public Player getPlayer(int id) {
+
+        Optional<Player> tempPlayer = repo.findById(id);
+
+        Player p = null;
+
+        if(tempPlayer.isPresent()) {
+            p = tempPlayer.get();
+        } else {
+            throw new RuntimeException("Player with id " + id + " not found.");
+        }
+
+        return p;
+    }
 
     public List<Player> getAllPlayers(){
         return repo.findAll();
