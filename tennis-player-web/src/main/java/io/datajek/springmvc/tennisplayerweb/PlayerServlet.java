1f6f29cf0cca9691acc1246e6df6c3439003ab30
SebaRodriguezzz <sebas.r1070@gmail.com>
2023-12-01 01:29:27 -0300

    Create JSP file

--- tennis-player-web/src/main/java/io/datajek/springmvc/tennisplayerweb/PlayerServlet.java
+++ tennis-player-web/src/main/java/io/datajek/springmvc/tennisplayerweb/PlayerServlet.java
@@ -3,6 +3,7 @@ package io.datajek.springmvc.tennisplayerweb;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -12,15 +13,7 @@ import javax.servlet.http.HttpServletResponse;
 public class PlayerServlet extends HttpServlet {
 
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        PrintWriter writer = response.getWriter();
-        writer.println("<html>");
-        writer.println("<head>" +
-                "<title>Player DB</title>" +
-                "</head>");
-        writer.println("<body>" +
-                "<H2>Welcome to the Tennis Players database!</H2>" +
-                "</body>");
-        writer.println("</html>");
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        request.getRequestDispatcher("/WEB-INF/views/welcome.jsp").forward(request, response);
     }
 }
---
+++ tennis-player-web/src/main/webapp/WEB-INF/views/welcome.jsp
@@ -0,0 +1,11 @@
+
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <meta charset="ISO-8859-1">
+    <title>Tennis Player DB</title>
+</head>
+<body>
+<h2>Welcome to the tennis player database!</h2>
+</body>
+</html>
